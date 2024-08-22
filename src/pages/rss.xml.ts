d54f7e1fc6d9668a22f098e93f7a5450dac2de0b
Emilio Navarro <sainfuw@outlook.com>
2024-08-22 19:10:29 -0400

    add esteroids for rss file

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,10 @@
 import rss from '@astrojs/rss'
 import type { APIRoute } from 'astro'
 import { getCollection, type CollectionEntry } from 'astro:content'
+import MarkdownIt from 'markdown-it'
+import sanitizeHtml from 'sanitize-html'
+
+const parser = new MarkdownIt()
 
 export const GET: APIRoute = async ({ params, request, site }) => {
   const blogs: CollectionEntry<'blog'>[] = await getCollection('blog')
@@ -9,11 +13,28 @@ export const GET: APIRoute = async ({ params, request, site }) => {
     title: "Emilio's Blog",
     description: "A humble Astronaut's guide to the stars",
     site: site ?? '',
-    items: blogs.map(({ data, slug }) => ({
+
+    xmlns: {
+      media: 'http://search.yahoo.com/mrss/',
+    },
+
+    items: blogs.map(({ data, slug, body }) => ({
       title: data.title,
       pubDate: data.date,
       description: data.description,
       link: `/posts/${slug}`,
+
+      content: sanitizeHtml(parser.render(body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+      }),
+
+      customData: `<media:content
+        type="image/${data.image.format === 'jpg' ? 'jpeg' : 'png'}"
+        width="${data.image.width}"
+        height="${data.image.height}"
+        medium="image"
+        url="${site + data.image.src}" />
+      `,
     })),
   })
 }
